7a8d4c38fa1d2a1d951fdafeb883a9c15b036636
gsn <gordejsamborskij@gmail.com>
2026-01-07 13:04:30 +0200

    work.py

---
+++ homework/work.py
@@ -0,0 +1,52 @@
+#number = int(input("Введіть перше число:"))
+#number1 = int(input("Введіть друге число:"))
+#number2 = int(input("Введіть третє число:"))
+#print(number + number1 + number2)
+
+#number1 = float(input("Введіть довжину першїо діагоналі: "))
+#number2 = float(input("Введіть довжину другої діагоналі: "))
+#S = (number1 * number2) / 2
+#print(S)
+
+
+
+
+# s = float(input("Введіть зарплату за місяць: "))
+# c = float(input("Введіть суму щомісячного платежу за кредитом: "))
+# u = float(input("Введіть заборгованість за комунальні послуги: "))
+# money = s - c - u
+# print(money)
+
+# d = float(input("Введіть відстань у кілометрах: "))
+# fuel1 = float(input("Введіть витрату палива на 100 км: "))
+# fuel2 = float(input("Введіть ціун за 1 літр бензину: "))
+# used = (d * fuel1) / 100
+# cost = used * fuel2
+# print(cost)
+
+
+
+# rahunok = float(input("Введіть загальну суму рахунку: "))
+# people = int(input("Введіть кількість осіб: "))
+# tip = rahunok * 0.15
+# total = rahunok + tip
+# per_person = total / people
+# print("Чайові:", tip)
+# print("Сума з чайовими:", total)
+# print("Сума на одну людину:", per_person)
+
+
+per_day = float(input("Введіть вартість оренди автомобіля за день: "))
+days = int(input("Введіть кількість днів оренди: "))
+d = float(input("Введіть суму застави: "))
+rent = per_day * days
+to_pay = rent + d
+cost_per_day = rent / days
+print("Загальна сума до оплати (з урахуванням застави):", to_pay)
+print("Сума, яку користувач витратить після повернення авто:", rent)
+print("Вартість оренди за один день:", cost_per_day)
+
+
+
+
+
