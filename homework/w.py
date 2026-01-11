41dcd46451b0454cf7f0d96bb30a6fceff40261d
gsn <gordejsamborskij@gmail.com>
2026-01-11 14:30:36 +0200

    w

--- homework/w.py
+++ homework/w.py
@@ -139,29 +139,34 @@ else:
 #     print(number2)
 
 
-number1 = int(input('Введіть число:'))
-number2 = int(input('Введіть число:'))
+# number1 = int(input('Введіть число:'))
+# number2 = int(input('Введіть число:'))
 
-operation = input('Оберіть операцію (+ - * /): ')
+# operation = input('Оберіть операцію (+ - * середнє): ')
 
-if operation == '+':
-    print(f'{number1} + {number2} = {number1 + number2}')
-elif operation == '-':
-    print(f'{number1} - {number2} = {number1 - number2}')
-elif operation == '*':
-    print(f'{number1} * {number2} = {number1 * number2}')
-elif operation == '/':
-    print(f'{number1} / {number2} = {number1 / number2}')
+# if operation == '+':
+#     print(f'{number1} + {number2} = {number1 + number2}')
+# elif operation == '-':
+#     print(f'{number1} - {number2} = {number1 - number2}')
+# elif operation == '*':
+#     print(f'{number1} * {number2} = {number1 * number2}')
+# elif operation == 'середнє':
+#     print(f'({number1} + {number2}) / 2 = {(number1 + number2) / 2}')
+# else:
+#     print('Некоректна операція')
+
+
+sum = float(input('сума: '))
+v = input('валюта: ')
+kurs = float(input('курс: '))
+if v == "євро":
+    print(f'{sum / kurs}')
+elif v == 'гривні':
+    print(f'{sum / kurs}')
+elif v == 'фунти':
+    print(f'{sum / kurs}')
 else:
-    print('Некоректна операція')
-
-
-
-
-
-
-
-
+    print('Error')
 
 
 
