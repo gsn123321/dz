c33317b51ae6c8e4cf89328aea4bc6fc8fda6fbf
gsn <gordejsamborskij@gmail.com>
2026-01-13 16:59:31 +0200

    homr

--- homework/home1.py
+++ homework/home1.py
@@ -37,7 +37,6 @@
 # else:
 #     print('Некоректна операція')
 
-
 # number = int(input("число від 5 до 1: "))
 # if number == 1:
 #     print("Дуже погано.")
@@ -52,26 +51,44 @@
 # else:
 #     print('нету такой оценки')
 
-metr = float(input('метры: '))
-option = input('милі, дюйми, ярди чи усе разом, сантиметри,  кілометри:')
-if option == 'милі':
-    print(f'{metr / 1600}')
-elif option == 'дюйми':
-    print(f'{metr * 40}')
-elif option == 'ярди':
-    print(f'{metr * 1.1}')
-elif option == 'усе разом':
-    print(f'{metr / 1600}')
-    print(f'{metr * 40}')
-    print(f'{metr * 1.1}')
-elif option == 'сантиметри':
-    print(f'{metr * 100}')
-elif option == 'кілометри':
-    print(f'{metr / 1000}')
-else:
-    print('error')
+# metr = float(input('метры: '))
+# option = input('милі, дюйми, ярди чи усе разом, сантиметри,  кілометри:')
+# if option == 'милі':
+#     print(f'{metr / 1600}')
+# elif option == 'дюйми':
+#     print(f'{metr * 40}')
+# elif option == 'ярди':
+#     print(f'{metr * 1.1}')
+# elif option == 'усе разом':
+#     print(f'{metr / 1600}')
+#     print(f'{metr * 40}')
+#     print(f'{metr * 1.1}')
+# elif option == 'сантиметри':
+#     print(f'{metr * 100}')
+# elif option == 'кілометри':
+#     print(f'{metr / 1000}')
+# else:
+#     print('error')
 
-
+# number1 = int(input('Введіть число:'))
+# number2 = int(input('Введіть число:'))
+# operation = input('Оберіть операцію (+, -, *, /, залишок, степінь): ')
+# if operation == '+':
+#     print(f'{number1} + {number2} = {number1 + number2}')
+# elif operation == 'залишок':
+#     print(f'{number1} % {number2} = {number1 % number2}')
+# elif operation == '-':
+#     print(f'{number1} - {number2} = {number1 - number2}')
+# elif operation == 'стапінь':
+#     print(f'{number1} ** {number2} = {number1 ** number2}')
+# elif operation == '*':
+#     print(f'{number1} * {number2} = {number1 * number2}')
+# elif operation == '/':
+#     if number2 == 0:
+#         print('Не можна ділити на нуль')
+#     print(f'{number1} / {number2} = {number1 / number2}')
+# else:
+#     print('Некоректна операція')
 
 
 
