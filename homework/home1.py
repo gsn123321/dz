c4fa3383b48c86de15c6d111f84c2d30112bf010
gsn <gordejsamborskij@gmail.com>
2026-01-13 16:29:21 +0200

    home1

---
+++ homework/home1.py
@@ -0,0 +1,53 @@
+# number1 = int(input('Введіть число:'))
+# number2 = int(input('Введіть число:'))
+# number3 = int(input('Введіть число:'))
+# operation = input('Оберіть операцію (+  *): ')
+# if operation == '+':
+#     print(f'{number1} + {number2} + {number3} = {number1 + number2 + number3}')
+# elif operation == '*':
+#     print(f'{number1} * {number2} * {number3} = {number1 * number2 * number3}')
+# else:
+#     print('Некоректна операція')
+
+# number1 = int(input('Введіть число:'))
+# number2 = int(input('Введіть число:'))
+# number3 = int(input('Введіть число:'))
+# operation = input('Оберіть операцію (максимум, мінімум, середнє): ')
+
+# if operation == 'максимум':
+#     if number1 - number2 > 0 and number2 - number3 > 0:
+#         print(number1)
+#     elif number1 - number2 < 0 and number2 - number3 > 0:
+#         print(number2)
+#     elif number1 - number2 < 0 and number2 - number3 < 0:
+#         print(number3)
+#     else:
+#         print('error')
+# elif operation == 'мінімум':
+#     if number1 - number2 < 0 and number2 - number3 < 0:
+#         print(number1)
+#     elif number1 - number2 > 0 and number2 - number3 < 0:
+#         print(number2)
+#     elif number1 - number2 > 0 and number2 - number3 > 0:
+#         print(number3)
+#     else:
+#         print('error')
+# elif operation == 'середнє':
+#     print(f'({number1} + {number2} + {number3}) / 3 = {(number1 + number2 + number3) / 3}')
+# else:
+#     print('Некоректна операція')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
