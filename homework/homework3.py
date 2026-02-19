f365dc35b532cbc5850f624e18b2573fcce35dcf
gsn <gordejsamborskij@gmail.com>
2026-02-19 17:01:57 +0200

    qwer

---
+++ homework/homework3.py
@@ -0,0 +1,61 @@
+# numbers = input("числа через пробіл: ").split()
+# n = int(input("кількість позицій: "))
+
+# numbers2 = numbers[-n:] + numbers[:-n]
+
+# print(numbers2)
+
+
+import random
+
+list1 = []
+list2 = []
+
+for i in range(10):
+    list1.append(random.randint(1, 20))
+    list2.append(random.randint(1, 20))
+
+list3 = list1 + list2
+print(list3)
+
+list4 = []
+for num in list3:
+    if num not in list4:
+        list4.append(num)
+print(list4)
+
+list4 = []
+for num in list1:
+    if num in list2 and num not in list4:
+        list4.append(num)
+print(list4)
+
+list4 = []
+for num in list1:
+    if num not in list2:
+        list4.append(num)
+for num in list2:
+    if num not in list1:
+        list4.append(num)
+print(list4)
+
+list4 = [min(list1), max(list1), min(list2), max(list2)]
+print(list4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
