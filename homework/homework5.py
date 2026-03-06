103d80826c4a45c2a4e4ae4565169e7ea2278402
gsn <gordejsamborskij@gmail.com>
2026-03-06 21:24:22 +0200

    qwe

---
+++ homework/homework5.py
@@ -0,0 +1,38 @@
+# def text():
+#     print('\'Dont compare yourself with anyone in this world...\n   if you do so, you are insulting yourself.\' \n      Bill Gates')
+# text()
+
+# def numbers(num1, num2):
+#     for i in range(num1, num2):
+#         if i % 2 == 0:
+#             print(i)
+# numbers(3,23)
+
+# def square(a,b,c):
+#     if a == True:
+#         for i in range(b):
+#             print(b * c)
+#     if a == False:
+#         for i in range(b):
+#             for j in range(b):
+#                 if i == 0 or i == b-1 or j == 0 or j == b-1:
+#                     print(c, end=" ")
+#                 else:print(" ", end=" ")
+#             print()
+# square(False, 7, "#")
+
+# def number(a):
+#     print(len(str(a)))
+# number(12345)
+
+# def number(a):
+#     a1 = a[3:]
+#     a2 = a[:3]
+#     a2 = a2[::-1]
+#     if a1 == a2:
+#         return True 
+#     if a1 != a2:
+#         return False
+#     else:print('no')
+
+# print(number(input("Введіть число: ")))
